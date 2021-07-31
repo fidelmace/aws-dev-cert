24c5a7e032a5907926e6d093ce6adbcbd9cb8715
Fidel Macedonio <fidelmace@hotmail.com>
2021-07-31 12:31:08 -0500

    Clase 2 aws2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola mundo AWS <code>src/App.js</code>desde AWS Amplify.
+          Hola mundo AWS <code>src/App.js</code>desde AWS Amplify FMM.
         </p>
         <a
           className="App-link"
